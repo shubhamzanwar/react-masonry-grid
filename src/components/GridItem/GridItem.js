298941be5e7ffe8fe092c6ba6aa1d682128e0cfa
ShubhamZanwar <zanwar.shubham@gmail.com>
2019-09-03 19:33:18 +0530

    grid: creating the grid item

---
+++ src/components/GridItem/GridItem.js
@@ -0,0 +1,35 @@
+import React, {useRef, useContext, useEffect} from 'react';
+import {GridContext} from '../../context/Grid.context';
+
+export const GridItem = (props) => {
+
+    const gridItemRef = useRef();
+    const gridContext = useContext(GridContext);
+
+    useEffect(itemLoaded, []);
+
+    itemLoaded = () => {
+        const refHeight = gridItemRef.current.clientHeight;
+        const refWidth = gridItemRef.current.clientWidth;
+        const {columnWidth, rowHeight, gutter: rowGap} = gridContext;
+
+        const desiredHeight = (columnWidth * refHeight) / refWidth;
+        const rowSpan = Math.ceil((desiredHeight + rowGap) / (rowHeight + rowGap))
+
+        gridItemRef.current.style = {
+            height: `${desiredHeight}px`,
+            width: `${columnWidth}px`,
+            gridRowEnd: `span ${rowSpan}`
+        }
+    }
+
+    return (
+        <div 
+            ref={gridItemRef} 
+            style={{width: `${gridContext.columnWidth}px`, visibility: 'none'}}
+            onLoad={itemLoaded}
+        >
+            {props.children}
+        </div>
+    )
+}
